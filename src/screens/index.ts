e5d4c920c86fef1c0e517eaa4981c33046ff608e
Kubilay <skubilayalbayrak@gmail.com>
2022-01-02 01:38:28 +0300

    //

--- src/screens/index.ts
+++ src/screens/index.ts
@@ -1,15 +1,6 @@
 import MainScreen from './mainScreen/MainScreen';
 import BlogScreen from './blogScreen/BlogScreen';
 import SkillsScreen from './skillsScreen/SkillsScreen';
-import ContactScreen from './contactScreen/ContactScreen';
-import WorkScreen from './workScreen/WorkScreen';
 import AboutMeScreen from './aboutmeScreen/AboutMeScreen';
 
-export {
-  MainScreen,
-  BlogScreen,
-  SkillsScreen,
-  ContactScreen,
-  WorkScreen,
-  AboutMeScreen,
-};
+export { MainScreen, BlogScreen, SkillsScreen, AboutMeScreen };
